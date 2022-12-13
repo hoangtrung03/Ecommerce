dbd4f544643c7ca61eb2585a7af4474b64104087
vuhoangtrungbin0303 <hoangtrung@mobcec.com>
2022-12-13 17:01:31 +0700

    +update: change small header

--- src/components/Brand/Brand.tsx
+++ src/components/Brand/Brand.tsx
@@ -3,8 +3,8 @@ import { Fragment } from 'react'
 export default function Brand() {
   return (
     <Fragment>
-      <img src='src/assets/logo.svg' alt='Brave Ecommerce' title='Brave Ecommerce' width={32} height={32} />
-      <span className='fs-22 ml-3 font-bold text-secondary-1A162E'>Brave Ecommerce</span>
+      <img src='src/assets/logo.svg' alt='Ecommerce' title='Ecommerce' width={32} height={32} />
+      <span className='fs-22 ml-3 font-bold text-secondary-1A162E'>Ecommerce</span>
     </Fragment>
   )
 }
